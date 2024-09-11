7a9218967acbb394636b05cf4133a3458dc859a6
okonpatrick <okonpatrick2000@gmail.com>
2024-09-11 13:49:40 +0100

    firebase auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,109 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { AxiosComponent } from "./components/Api/AxiosMethod";
-//import RouterClass from "./components/react-router-class/lazy-loading/router-class";
-import { TailwindClass } from "./components/TaiwoClass/firstClass";
-import { NavBar } from "./components/TaiwoClass/Navbar";
-import { SignUp } from "./components/Forms/Signup";
-import UseReducerClass from './components/useReducerHook/useReducerBasic';
-import FuncComp from './components/useReducerHook/useReducer';
-import { ThemeComponent } from './components/manageTheme';
-//new router shipped to v6.4 of react-router-dom
-//here we create the router outside any React component 
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { AxiosComponent } from "./components/Api/AxiosMethod";
+// //import RouterClass from "./components/react-router-class/lazy-loading/router-class";
+// import { TailwindClass } from "./components/TaiwoClass/firstClass";
+// import { NavBar } from "./components/TaiwoClass/Navbar";
+// import { SignUp } from "./components/Forms/Signup";
+// import UseReducerClass from './components/useReducerHook/useReducerBasic';
+// import FuncComp from './components/useReducerHook/useReducer';
+// import { ThemeComponent } from './components/manageTheme';
+// //new router shipped to v6.4 of react-router-dom
+// //here we create the router outside any React component 
 
-const router = createBrowserRouter([
-  //notice the difference in the home route. keeping /api out of the children object makes it an independent route.
-  {
-    path: '/',
-    element: <>
-      <NavBar />
-      <TailwindClass />
-    </>,
-    children: [
-      {
-        path: 'class',
-        element: <TailwindClass />,
-      },
-    ],
-  },
-  {
-    path: 'signup',
-    element: <>
-      <NavBar />
-      <SignUp />
-    </>
-  },
-  {
-    path: 'product/:id',
-    element: <AxiosComponent />
-  },
-  {
-    path: 'product',
-    element: <AxiosComponent />
+// const router = createBrowserRouter([
+//   //notice the difference in the home route. keeping /api out of the children object makes it an independent route.
+//   {
+//     path: '/',
+//     element: <>
+//       <NavBar />
+//       <TailwindClass />
+//     </>,
+//     children: [
+//       {
+//         path: 'class',
+//         element: <TailwindClass />,
+//       },
+//     ],
+//   },
+//   {
+//     path: 'signup',
+//     element: <>
+//       <NavBar />
+//       <SignUp />
+//     </>
+//   },
+//   {
+//     path: 'product/:id',
+//     element: <AxiosComponent />
+//   },
+//   {
+//     path: 'product',
+//     element: <AxiosComponent />
 
-  },
-  {
-    path: 'cart',
-    element: <UseReducerClass />
+//   },
+//   {
+//     path: 'cart',
+//     element: <UseReducerClass />
 
-  },
-  {
-    path: 'cart/checkout',
-    element: <FuncComp />
+//   },
+//   {
+//     path: 'cart/checkout',
+//     element: <FuncComp />
 
-  },
+//   },
 
 
-]);
+// ]);
 
-export default function App() {
+// export default function App() {
 
-  return (
-    <ThemeComponent>
-      <div>
-      <RouterProvider router={router} />
-    </div>
-    </ThemeComponent>
+//   return (
+//     <ThemeComponent>
+//       <div>
+//       <RouterProvider router={router} />
+//     </div>
+//     </ThemeComponent>
     
-  );
-}
+//   );
+// }
 
 
 //Let's start the firebase as a new app.
-// App.js
-// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-// import { AuthProvider, useAuth } from "./components/useContextClass/AuthContext";
-// import SignUp from './components/useContextClass/signup';
-// import SignIn from './components/useContextClass/signin';
-// import Profile from './components/useContextClass/userprofile';
+//App.js
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from "./components/useContextClass/AuthContext";
+import SignUp from './components/useContextClass/signup';
+import SignIn from './components/useContextClass/signin';
+import Profile from './components/useContextClass/userprofile';
 
-// const ProtectedRoute = ({ children }) => {
-//   const { user, loading } = useAuth();
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useAuth();
 
-//   if (loading) return <p>Loading...</p>;
-//   if (!user) return <Navigate to="/login" />;
+  if (loading) return <p>Loading...</p>;
+  if (!user) return <Navigate to="/login" />;
 
-//   return children;
-// };
+  return children;
+};
 
-// const App = () => {
-//   return (
-//     <Router>
-//       <AuthProvider>
-//         <Routes>
-//           <Route path="/signup" element={<SignUp />} />
-//           <Route path="/login" element={<SignIn />} />
-//           <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-//         </Routes>
-//       </AuthProvider>
-//     </Router>
-//   );
-// };
+const App = () => {
+  return (
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/profile" element={
+            <ProtectedRoute>
+              <Profile />
+              </ProtectedRoute>
+            } />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
+};
 
-// export default App;
+export default App;
 
 
 
